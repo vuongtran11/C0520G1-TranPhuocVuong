7ee3740ab74b31dc6e538d6a9b4f3828faa41405
vuongtran11 <vuongsky3@gmail.com>
2020-06-09 01:05:23 +0700

    bt

---
+++ Module1/6.BienKieuDuLieuToanTu/ThucHanh/ToanTu.js
@@ -0,0 +1,8 @@
+let inputwidth;
+let inputhieght;
+inputwidth = prompt('Enter the width:');
+inputhieght = prompt('Enter the height');
+let width = parseInt(inputwidth);
+let hight = parseInt(inputhieght);
+let area = width * hight ;
+document.write("The area is = "+ area);
