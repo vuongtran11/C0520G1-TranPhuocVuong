9e9c40d4943c8bfecb608b132863d90f6172c6e1
vuongtran11 <vuongsky3@gmail.com>
2020-07-10 08:38:56 +0700

    bt

---
+++ Module2/src/dsa_danhsach/bai_tap/MyArrayListTest.java
@@ -0,0 +1,55 @@
+package dsa_danhsach.bai_tap;
+
+public class MyArrayListTest {
+    public static class Student {
+        private int id;
+        private String name;
+
+        public Student() {
+        }
+
+        public Student(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+    public static void main(String[] args) {
+        Student a=new Student(1,"Huy");
+        Student b=new Student(2,"Hoa");
+        Student c=new Student(3,"Lan");
+        Student d=new Student(4,"Mai");
+        Student e=new Student(5,"My");
+        Student f=new Student(6,"My");
+        MyArrayList<Student>studentMyArrayList=new MyArrayList<>();
+        studentMyArrayList.add(a);
+        studentMyArrayList.add(b);
+        studentMyArrayList.add(c);
+        studentMyArrayList.add(d);
+        studentMyArrayList.add(e);
+        studentMyArrayList.add(f,2);
+        // studentMyArrayList.clear();
+        /*for(int i=0;i<studentMyArrayList.size();i++){
+            Student student=(Student) studentMyArrayList.elements[i];
+            System.out.println(student.getId());
+            System.out.println(student.getName());
+        }*/
+        System.out.println(studentMyArrayList.size());
+    }
+}
